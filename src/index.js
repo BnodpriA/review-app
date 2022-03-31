06ba1fe2adde420008d67ed939f40da094077438
Binod Chaudhary <bunnycdy@gmail.com>
2022-03-31 16:13:41 +0545

    index.css file imported and tested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import * as ReactDomClient from "react-dom/client"; //interact with DOM
 import App from "./App"; //import App.js component
+import './index.css'; //import css file
 
 const rootContainer = document.getElementById("root");
 const root  = ReactDomClient.createRoot(rootContainer);
