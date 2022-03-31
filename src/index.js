7d05603ca6b7ee706689bd4d4fb60d23268c1ac3
Binod Chaudhary <bunnycdy@gmail.com>
2022-03-31 16:10:38 +0545

    StrictMode enabled in while returning App component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-import React from "react";
-//we will replace second namespace with third from React v18
-//import ReactDOM from "react-dom"; //interact with DOM 
+import React, { StrictMode } from "react";
 import * as ReactDomClient from "react-dom/client"; //interact with DOM
-
-/**
- * this code will be replaced by the code below
- * 
- * ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById("root")
-)
-**/
+import App from "./App"; //import App.js component
 
 const rootContainer = document.getElementById("root");
 const root  = ReactDomClient.createRoot(rootContainer);
-root.render(<h1>I ❤️ Chicken</h1>);
+root.render(
+    <React.StrictMode> //StrictMode is a React feature that helps with debugging
+        <App />
+    </React.StrictMode>
+);
 
 
