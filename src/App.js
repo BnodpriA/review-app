c02e2fa720f44312a5089a9c68eb76754e73d459
Binod Chaudhary <bunnycdy@gmail.com>
2022-03-31 15:48:34 +0545

    App.js component modified to return JSX

---
+++ src/App.js
@@ -0,0 +1,11 @@
+/**
+ * App() function created
+ * @return JSX (JavaScript XML) file
+ * 
+ */
+function App() {
+    return(
+        <h1>Hello from the App Component</h1>
+    )
+}
+export default App;
