82aca179348234186d68fd1700c006e5b2f56df6
Binod Chaudhary <bunnycdy@gmail.com>
2022-04-01 10:36:26 +0545

    created list variable and return in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 function App() { 
-    return React.createElement(
-        'div', { className: 'container' },
-        React.createElement('h1', {}, 'Hello World'),
-        React.createElement('p', { className: 'bold-paragraph'}, 'This is my first React app')
+    const title = 'This is a title';
+    const subtitle = 'This is a subtitle';
+    const comments = [
+        {id: 1, text: 'This is comment 1'},
+        {id: 2, text: 'This is comment 2'},
+        {id: 3, text: 'This is comment 3'},
+        {id: 4, text: 'This is comment 4'},
+        {id: 5, text: 'This is comment 5'},
+
+    ];
+    return(
+        <div className="container">
+            <h1>{title.toLocaleUpperCase()}</h1>
+            <p>{subtitle}</p>
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
 export default App;
