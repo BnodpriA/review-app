50cde0b8d658a6b7a84323c72363e5be9cdf8662
Binod Chaudhary <bunnycdy@gmail.com>
2022-04-01 15:11:12 +0545

    Feedback component created and imported

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FeedbackItem() {
+  return (
+    <div className="card">
+        <div className="num-display">10</div>
+        <div className="text-display">
+            Text will be displayed here
+        </div>
+    </div>
+  )
+}
+
+export default FeedbackItem
