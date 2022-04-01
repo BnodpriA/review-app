f281d71e08623d62039981055ef96f3d52fd2131
Binod Chaudhary <bunnycdy@gmail.com>
2022-04-01 12:13:42 +0545

    Header component modified

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div className="container">
+            <h1>My App</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
