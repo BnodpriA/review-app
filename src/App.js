477b186d5887f569e1ed1e045b9095f7d9efb7b3
Binod Chaudhary <bunnycdy@gmail.com>
2022-04-01 11:46:50 +0545

    set commentBlock as variable

--- src/App.js
+++ src/App.js
@@ -11,22 +11,23 @@ function App() {
 
     ];
     const loading = false;
-    const showComments = false;
+    const showComments = true;
     if(loading) return <h1>Loading...</h1>
+    const commentBlock = (
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+    )
     return(
         <div className="container">
             <h1>{title.toLocaleUpperCase()}</h1>
             <p>{subtitle}</p>
-            {showComments && (
-                <div className="comments">
-                    <h3>Comments ({comments.length})</h3>
-                    <ul>
-                        {comments.map((comment, index) => (
-                            <li key={index}>{comment.text}</li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+            {showComments && commentBlock}
             
         </div>
     )
